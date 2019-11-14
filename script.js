61054d05431b2ed6ef716e41d4559650c7a98395
S-Coggin <53793753+S-Coggin@users.noreply.github.com>
2019-11-14 10:40:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-// alert("CONNECTED");
+alert("CONNECTED");
 
-// for (let i = -10; i <= 20; i++) {
-//     console.log(i);
-// }
+for (let i = -10; i <= 20; i++) {
+    console.log(i);
+}
 
-// for(let i=10; i<=40; i++) {
-//     if(i % 2 === 0) {
-//         console.log(i);
-//     }
-// }
+for(let i=10; i<=40; i++) {
+    if(i % 2 === 0) {
+        console.log(i);
+    }
+}
     
 
-// for(let i=300; i<=333; i++) {
-//     if(i % 2 !== 0) {
-//         console.log(i);
-//     }
-// } 
+for(let i=300; i<=333; i++) {
+    if(i % 2 !== 0) {
+        console.log(i);
+    }
+} 
 
 
-// for (let i = 5; i <= 50; i++) {
-//     if(i % 3 === 0 && i % 5 === 0) {
-//         console.log(i);
-//     }
+for (let i = 5; i <= 50; i++) {
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log(i);
+    }
     
-// }
+}
